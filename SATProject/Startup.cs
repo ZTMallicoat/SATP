ad6c4f07166291beec9cc497b3842c0cd4f25653
ZTMallicoat <ZTMallicoat@gmail.com>
2020-11-11 11:13:53 -0600

    Add project files.

---
+++ SATProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SATProject.Startup))]
+namespace SATProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
